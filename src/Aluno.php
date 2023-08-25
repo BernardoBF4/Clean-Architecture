3481667e3fd9d5fad4d4c552d2ad4970433d77e7
BernardoBF4 <bernardo.fantin@gmail.com>
2023-08-24 23:13:55 -0300

    ✨ feat(arquitetura: aluno): criado um named constructor para a classe

--- src/Aluno.php
+++ src/Aluno.php
@@ -4,13 +4,26 @@ namespace Arquitetura;
 
 class Aluno
 {
-  private int $cpf; // Como esta classe terá instâncias que serão diferentes entre si, ela é uma entidade, e, portanto, precisa de um identificador: o CPF.
+  private Cpf $cpf; // Como esta classe terá instâncias que serão diferentes entre si, ela é uma entidade, e, portanto, precisa de um identificador: o CPF.
   private string $nome;
   private Email $email;
   private array $telefones;
 
-  public function adicionarTelefones(string $ddd, string $numero): void
+  public function __construct(Cpf $cpf, string $nome, Email $email)
+  {
+    $this->cpf = $cpf;
+    $this->nome = $nome;
+    $this->email = $email;
+  }
+
+  public static function comCpfNomeEEmail(string $cpf, string $nome, string $email): self
+  {
+    return new Aluno(new Cpf($cpf), $nome, new Email($email));
+  }
+
+  public function adicionarTelefones(string $ddd, string $numero): self
   {
     $this->telefones[] = new Telefone($ddd, $numero);
+    return $this;
   }
 }
